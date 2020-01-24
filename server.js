fef74e87e8e5dabc6cb43505d1c57e8906d5a192
brandenlacour <brandenlacour@gmail.com>
2020-01-24 17:01:17 -0600

    added home ejs

--- server.js
+++ server.js
@@ -2,14 +2,19 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT
-
-
+const bodyParser = require('body-parser')
+//database
 require('./db/db')
+
+
+//middleware
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
 
 
 
 app.get('/', (req, res) => {
-	res.send('Hello World')
+	res.render('home.ejs')
 })
 
 
