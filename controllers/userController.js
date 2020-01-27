0f49cf3147a260543e32198691b3b376af5e7c30
Nehemias Alcantara <nemiasalc56@gmail.com>
2020-01-26 20:57:54 -0600

    user can see their profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,41 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
+const cloudinary = require('cloudinary').v2
+const multer = require('multer')
+const upload = multer({ dest: "uploads/"})
+
 
 
 
 // GET profile route
 router.get('/', async (req, res, next) => {
 	try {
-		
+		const foundUser = await User.findById(req.session.userId)
+		console.log(foundUser);
 
-		res.render('user/profile.ejs')
+		const userPhotoUrl = cloudinary.url(`${foundUser.imageId}.jpg`)
+
+		res.render('user/profile.ejs', {
+			user: foundUser,
+			imageUrl: userPhotoUrl
+		})
 	} catch(err) {
 		next(err)
 	}
 })
 
 
+// GET profile route
+router.get('/image', async (req, res, next) => {
+	try {
+		const foundUser = await User.findById(req.session.userId)
+
+		res.send(cloudinary.url(foundUser.imageId))
+	} catch(err) {
+		next(err)
+	}
+})
 
 
 
