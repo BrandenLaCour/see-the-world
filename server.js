171f0cbcd3014b3adc6bcb420e38a73186129877
Nehemias Alcantara <nemiasalc56@gmail.com>
2020-01-29 13:13:35 -0600

    styling header

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
 		res.locals.username = req.session.username
 		res.locals.loggedIn = req.session.loggedIn
 		res.locals.message = req.session.message
+		res.locals.photoUrl = req.session.photoUrl
 	} else {
 		res.locals.userId = false
 		res.locals.loggedIn = false
